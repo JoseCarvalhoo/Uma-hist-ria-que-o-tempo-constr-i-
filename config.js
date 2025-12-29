1485f7bdcdff13210d07ab69f5131abdabd552ce
JoseCarvalhoo <josecarvalho11j@gmail.com>
2025-12-29 13:32:08 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+var config = {
+    lines: [
+"Para a minha amada Emanuella,",
+"Este é apenas um teste do nosso amor,",
+"Que cresce a cada segundo,",
+"Desde daquele dia 28 de abril, eu tive a         certeza que você era uma pessoa especial.",
+"Sinto muito pelo o acontecido, eu não acreditava que todo casal passava por isso.",
+"Espero que você goste desta surpresa,",
+"Pois você merece o mundo inteiro!",
+"Com amor, José Osterno.",
+    ],
+    // Data: 28 de Abril às 10:46
+    date: "2025-04-28T10:46:00",
+    names: ["José Osterno", "Emanuella"]
+}
