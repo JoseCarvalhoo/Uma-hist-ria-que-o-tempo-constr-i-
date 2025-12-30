7e4040e929c7345101db9b1d3860de7c407f9d1e
O Tempo <josecarvalho11j@gmail.com>
2025-12-29 22:46:41 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 var config = {
     lines: [
 "Para a minha amada Emanuella,",
-"A saudade é o jeito que o coração encontra para dizer que o tempo não foi capaz de levar o que é nosso.A cerejeira continua a crescer, e eu continuo a admirar a mulher incrível que você é.",
-"Que cresce a cada segundo,",
-"Naquela manhã do dia 28 enquanto segurava sua mão, eu tive a certeza que você era uma pessoa especial.",
-"Uma mulher de coração nobre e com a verdadeira essência feminina.",
-"Garanto que haverá fases melhores para a gente.",      
-"Espero que você goste desta surpresa.",
-"Pois você merece o mundo inteiro!",
+"A saudade é o jeito que o coração encontra para dizer que o tempo não foi capaz de levar o que é nosso.",
+"A cerejeira continua a crescer, e eu continuo a admirar a mulher incrível que você é.",
+"No meio de qualquer tempestade, lembre-se que existe um jardim que construímos juntos. Se o peso do dia cansar, volta aqui. Meu apoio é silencioso, mas é eterno, assim como a nossa história.",
+"Existem dias de espera e dias de conquista. Enquanto o tempo cuida de tudo, eu cuido de estar aqui para você.",
+"O que é real nunca se apaga, apenas se fortalece no silêncio.",      
+"Que a calma de hoje te ajude a refletir sobre a força do que sentimos. A saudade é apenas o reflexo de algo que ainda vive.",
+"Saiba que nenhuma tempestade dura para sempre. Sigo aqui, guardando o nosso lugar e esperando o tempo florescer novamente",
 "Com amor, José Osterno.",
     ],
     // Data: 28 de Abril às 10:46
